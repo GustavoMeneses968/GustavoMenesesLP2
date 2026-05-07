011ea63d8d9bc74bf28d6823f215ffc380fac0c7
gustavo <gmeneses964@gmail.com>
2026-05-07 08:10:17 -0300

    criado a pasta do segundo bimestre e inicio da lista de manipulação de string

---
+++ segundoBimestre/listaString/Bee1871.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class Bee1871 {
+
+    final static Scanner SCANNER  = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        while(true){
+            int valor1 = SCANNER.nextInt();
+            int valor2 = SCANNER.nextInt();
+            if(valor1 == 0 && valor2 == 2){
+                break;
+            }
+            int soma = valor1 + valor2;
+            String st = " ";
+            st = soma + st;
+            char[] vet;
+            for(int i = 0; i < st.length(); i++){
+                if(){
+
+                }
+            }
+        }
+    }
+}
